5a3b4f3da385fdbfe7e303f6a7f3505bc0c30326
Menaid <134172054+Menaid@users.noreply.github.com>
2023-09-27 21:30:38 +0200

    Added for loop and scoreboeard. Have a finished game but not polished.

--- Drag.cs
+++ Drag.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TwoPlayer
+﻿namespace TwoPlayer
 {
     public enum Drag
     {
-        Sten,
-        Sax,
-        Påse
+        sten,
+        sax,
+        påse
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,10 +7,6 @@ spelare spelare1 = null;
 spelare spelare2 = null;
 
 string val = string.Empty;
-string drag1 = string.Empty;
-string drag2 = string.Empty;
-int points1 = 0;
-int points2 = 0;
 
 bool alternativ = true;
 while (alternativ)
@@ -30,57 +26,156 @@ while (alternativ)
 bool OneOrTwo = true; ;
 bool pve = true;
 
-while (OneOrTwo)
+while (true)
 {
-    if (val == "1")
+
+    while (OneOrTwo)
     {
-        Console.WriteLine("Vänligen ange vilket namn spelare 1 har.");
-        spelare1 = new spelare(Console.ReadLine());
-        spelare2 = new spelare("Bot", true);
+        if (val == "1")
+        {
+            Console.WriteLine("Vänligen ange vilket namn spelare 1 har.");
+            spelare1 = new spelare(Console.ReadLine());
+            spelare2 = new spelare("Botten", true);
+        }
+        else if (val == "2")
+        {
+            Console.WriteLine("Vänligen ange vilket namn spelare 1 har.");
+            spelare1 = new spelare(Console.ReadLine());
+            Console.WriteLine("Vänligen ange nu vilket namn spelare 2 har.");
+            spelare2 = new spelare(Console.ReadLine());
+            pve = false;
+        }
+        OneOrTwo = false;
     }
-    else if (val == "2")
+
+    Random random = new Random();
+
+    var firstRound = random.Next(0, 2);
+
+    spelare firstPlayer = null;
+    spelare secondPlayer = null;
+
+    if (firstRound == 1)
     {
-        Console.WriteLine("Vänligen ange vilket namn spelare 1 har.");
-        spelare1 = new spelare(Console.ReadLine());
-        Console.WriteLine("Vänligen ange nu vilket namn spelare 2 har.");
-        spelare2 = new spelare(Console.ReadLine());
-        pve = false;
-    }
-    OneOrTwo = false;
-}
-
-Random random = new Random();
-
-var firstRound = random.Next(0, 2);
-
-spelare firstPlayer = null;
-spelare secondPlayer = null;
-
-if (firstRound == 1)
-{
-    firstPlayer = spelare1;
-    secondPlayer = spelare2;
-}
-else
-{
-    firstPlayer = spelare2;
-    secondPlayer = spelare1;
-}
-
-
-
-for (int i = 0; i < 3; i++)
-{
-
-
-    if (firstPlayer.isBot)
-    {
-        var drag = (Drag)random.Next(0, 3);
+        firstPlayer = spelare1;
+        secondPlayer = spelare2;
     }
     else
     {
-        Console.WriteLine("Välj ett av alternativen sten, sax, påse.");
-        string valDrag = Console.ReadLine();
+        firstPlayer = spelare2;
+        secondPlayer = spelare1;
+    }
+
+    Console.WriteLine("Spelare " + firstPlayer.name + " börjar.");
+
+
+    for (int i = 0; i < 3; i++)
+    {
+        var drag1 = Drag.påse;
+        var drag2 = Drag.påse;
+
+        if (firstPlayer.isBot)
+        {
+            drag1 = (Drag)random.Next(0, 3);
+        }
+        else
+        {
+            while (true)
+            {
+                Console.WriteLine("Spelare " + firstPlayer.name + " välj ett av alternativen sten, sax, påse.");
+                string valDrag = Console.ReadLine().ToLower();
+                if (Enum.TryParse<Drag>(valDrag, true, out var parsed))
+                {
+                    drag1 = parsed;
+                    break;
+                }
+            }
+
+            if (!pve)
+            {
+                Console.Clear();
+            }
+
+        }
+
+
+        if (secondPlayer.isBot)
+        {
+            drag2 = (Drag)random.Next(0, 3);
+        }
+        else
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Spelare " + secondPlayer.name + " välj ett av alternativen sten, sax, påse.");
+                string valDrag = Console.ReadLine().ToLower();
+                if (Enum.TryParse<Drag>(valDrag, true, out var parsed))
+                {
+                    drag2 = parsed;
+                    break;
+                }
+            }
+            if (!pve)
+            {
+                Console.Clear();
+            }
+
+        }
+
+
+        if (drag1 == drag2)
+        {
+            Console.WriteLine("Det blir oavgjort");
+        }
+        else if (drag1 == Drag.sten && drag2 == Drag.sax)
+        {
+            firstPlayer.points++;
+            Console.WriteLine("Grattis, " + firstPlayer.name + " fick ett poäng");
+        }
+        else if (drag1 == Drag.sax && drag2 == Drag.påse)
+        {
+            firstPlayer.points++;
+            Console.WriteLine("Grattis, " + firstPlayer.name + " fick ett poäng");
+        }
+        else if (drag1 == Drag.påse && drag2 == Drag.sten)
+        {
+            firstPlayer.points++;
+            Console.WriteLine("Grattis, " + firstPlayer.name + " fick ett poäng");
+        }
+        else
+        {
+            secondPlayer.points++;
+            Console.WriteLine("Grattis, " + secondPlayer.name + " fick ett poäng");
+        }
+
+    }
+
+    Console.WriteLine("Spelare " + firstPlayer.name + " fick: " + firstPlayer.points);
+    Console.WriteLine("Spelare " + secondPlayer.name + " fick: " + secondPlayer.points);
+
+    if (firstPlayer.points > secondPlayer.points)
+    {
+        Console.WriteLine("Grattis " + firstPlayer.name + " vann.");
+    }
+    else if (secondPlayer.points > firstPlayer.points)
+    {
+        Console.WriteLine("Grattis " + secondPlayer.name + " vann.");
+    }
+    else
+    {
+        Console.WriteLine("Hepp det blev oavgjort!");
+    }
+
+    Console.WriteLine("Vill ni spela igen");
+    var spelaIgen = Console.ReadLine().ToLower();
+    if (spelaIgen == "ja")
+    {
+        OneOrTwo = true;
+        Console.Clear();
+    }
+    else
+    {
+        break;
     }
 }
-Console.WriteLine("Spelare " + firstPlayer.name + " börjar.");
